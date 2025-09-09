41bc327c02c9397c2ac305bcf3a9496265c2baff
Daud <daud94237@gmail.com>
2025-09-09 15:13:25 +0800

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,8 @@
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = "Daud";
+        System.out.println("Nama : " + nama);
+        System.out.print("Nama : " + nama);
+        System.out.printf("%nNama : %s ", nama);
+    }
+}
