e3d4df5329392cf4db6027429cfdc9b701d9dc26
Daud <daud94237@gmail.com>
2025-09-10 16:43:28 +0800

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,12 @@
+public class Day3 {
+    public static void main(String[] args) {
+        // Ini adalah komentar satu baris
+
+
+        /*
+         * Ini adalah komentar
+         * yang lebih dari satu baris
+         * atau multi line comment
+         */
+    }
+}
