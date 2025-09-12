f61099f6a68de3aad3df559f523122ac0cec2719
Daud <daud94237@gmail.com>
2025-09-12 18:48:32 +0800

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,16 @@
+public class Day5 {
+    public static void main(String[] args) {
+
+        // Type Data Numeric (Byte, Short, Int, Long)
+
+        byte a = 20;
+        short b = 2000;
+        int c = 10000;
+        long d= 1200000000L;
+
+        System.out.println("Byte : " + a);
+        System.out.println("Short : " + b);
+        System.out.println("Int : " + c);
+        System.out.println("long : " + d);
+    }
+}
