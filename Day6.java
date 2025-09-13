11e3ff9f8c4a9e39c4bc6f74ca593964f2f20a5c
Daud <daud94237@gmail.com>
2025-09-13 17:53:28 +0800

    Add files via upload

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+    public static void main(String[] args) {
+        // tipe data pecahan
+        double berat = 48.3;
+        float Tinggi = 116.2f;
+        System.out.println("Berat Badan saya : " + berat);
+        System.out.println("Tinggi badan saya : " + Tinggi);
+    }
+}
+
