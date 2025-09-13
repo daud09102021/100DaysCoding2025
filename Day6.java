8630609d4f285001898038e3b6136c789e45ad0a
Daud <daud94237@gmail.com>
2025-09-13 17:54:44 +0800

    Day6.java

--- Day6.java
+++ Day6.java
@@ -2,9 +2,10 @@ public class Day6 {
     public static void main(String[] args) {
         // tipe data pecahan
         double berat = 48.3;
-        float Tinggi = 116.2f;
+        float Tinggi = 163.2f;
         System.out.println("Berat Badan saya : " + berat);
         System.out.println("Tinggi badan saya : " + Tinggi);
     }
 }
+
 
