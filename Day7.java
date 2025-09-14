2a5a39fb5bfa0cfa085b235c9ede8ee129e49de3
Daud <daud94237@gmail.com>
2025-09-14 19:10:52 +0800

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,13 @@
+public class Day7 {
+    public static void main(String[] args) {
+        // tipe data carakter dan boolean
+
+        // char hanya 1 karakter
+        char huruf = 'A';
+        System.out.println("Char: " + huruf);
+
+        // boolean hanya true atau false
+        boolean a = true;
+        System.out.println("Boolean: " + a);
+    }
+}
