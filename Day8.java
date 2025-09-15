d642483e9036735a7e9bc41ae9e023fe48abc75f
Daud <daud94237@gmail.com>
2025-09-15 21:22:34 +0800

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Day8 {
+    public static void main(String[] args) {
+
+        // Membuat variabel String
+        String nama = "Daud";
+        String Asal = "Mamasa";
+        String hobi = "Bermain Voly";
+
+        System.out.println("Halo Semua perkenalkan nama saya " + nama + ",\nsaya berasal dari " + Asal + ",\nhobi saya adalah " + hobi + ".");
+    }
+}
