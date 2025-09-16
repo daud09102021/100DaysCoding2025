747f2448c8e0d09614cf8d3ea28e7098c332b3ce
Daud <daud94237@gmail.com>
2025-09-16 20:52:26 +0800

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,21 @@
+public class Day9 {
+    public static void main(String[] args) {
+        
+    
+        // Deklarasi variabel
+        String nama ;
+        int umur;
+
+        // Inisialisasi variabel
+        nama = "Daud";
+        umur = 19;
+
+        System.out.println("Nama: " + nama + ", Umur: " + umur);
+
+        // Update nilai variabel
+        umur = 21;
+
+        System.out.println("Umur setelah update: " + umur);
+
+}
+}
