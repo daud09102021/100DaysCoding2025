103fba65c274f9eeeba5a9486af09fe18efd800a
Daud <daud94237@gmail.com>
2025-09-17 19:51:01 +0800

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,16 @@
+public class Day10 {
+    public static void main(String[] args) {
+        //Constanta (Final Variabel)
+        
+        // Deklarasi konstanta dengan final
+        final String nama = "Daud";
+        final int umur = 20;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+
+        // Jika mencoba mengubah nilainya → ERROR
+        // UMUR = 25;   //  Tidak boleh karena final
+    }
+}
+
