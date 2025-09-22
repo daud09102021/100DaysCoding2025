d0f2b06cf3e9606cb7d389d62dedf5a0d25c53e8
Daud <daud94237@gmail.com>
2025-09-22 19:46:04 +0800

    Add files via upload

---
+++ Day15.java
@@ -0,0 +1,10 @@
+public class Day15 {
+    public static void main(String[] args) {
+        // Opeeator aritmatika penjumlahan dan pengurangan
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Penjumlahan: " + (a + b)); 
+        System.out.println("Pengurangan: " + (a - b)); 
+    }
+}
