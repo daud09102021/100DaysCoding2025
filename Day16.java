de36c0db8099fb3b9e898c1c578d12a099089df7
Daud <daud94237@gmail.com>
2025-09-23 21:45:41 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class Day16 {
+    public static void main(String[] args) {
+      //Operator Aritmatika Perkalian (*) dan Pembagian (/). 
+        int angka1 = 10;
+        int angka2 = 5;
+
+        
+        int hasilKali = angka1 * angka2;
+        System.out.println("Hasil perkalian: " + hasilKali);
+      
+        int hasilBagi = angka1 / angka2;
+        System.out.println("Hasil pembagian: " + hasilBagi);
+    }
+}
