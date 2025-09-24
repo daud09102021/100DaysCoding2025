2da63b51b99f71147ae2bc2adfc68ddc2fe3e3ca
Daud <daud94237@gmail.com>
2025-09-24 17:29:05 +0800

    Add files via upload

---
+++ Day17.java
@@ -0,0 +1,10 @@
+public class Day17 {
+    public static void main(String[] args) {
+        // Operator Aritmatika Modulus atau Sisa Bagi (%)
+        
+        int a = 17;
+        int b = 5;
+        int hasil = a % b;
+        System.out.println("Hasil dari modulus = " + hasil);
+    }
+}
