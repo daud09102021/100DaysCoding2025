2c186b9d4e9c29e66f05144096b31040aa3c3984
Daud <daud94237@gmail.com>
2025-09-25 18:47:32 +0800

    Add files via upload

---
+++ Day18.java
@@ -0,0 +1,21 @@
+public class Day18 {
+    public static void main(String[] args) {
+        // Konversi otomatis (Dari tipe data kecil ke besar)
+
+        byte a = 100;
+        short b = a;
+        int c = b;
+        long d = c;
+        float e = d;
+        double f = e;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+        
+    }
+}
