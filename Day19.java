5c0472e9c50b8ab0a338fa3cf2dd59767a399684
Daud <daud94237@gmail.com>
2025-09-26 20:40:57 +0800

    Add files via upload

---
+++ Day19.java
@@ -0,0 +1,9 @@
+public class Day19 {
+    public static void main(String[] args) {
+        // Konversi manual/paksa (Dari tipe data besar ke kecil)
+        double d = 9.75;
+        int i = (int) d;   
+        System.out.println("double: " + d);
+        System.out.println("int   : " + i);
+    }
+}
