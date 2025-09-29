a1e89e06845a6b8c86619d713459aa50d43fcacf
Daud <daud94237@gmail.com>
2025-09-29 17:21:52 +0800

    Add files via upload

---
+++ Day22.java
@@ -0,0 +1,9 @@
+public class Day22 {
+    public static void main(String[] args) {
+        // Latihan: Program luas persegi
+        int sisi = 15;
+        int luasPersegi = sisi * sisi;
+        System.out.println("Sisi : " + sisi);
+        System.out.println("Luas Persegi : " + luasPersegi);
+    }
+}
