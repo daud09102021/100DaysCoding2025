afc22c5c997f1ba81fc0a30de67996a3448a5ba0
Daud <daud94237@gmail.com>
2025-09-30 18:28:35 +0800

    Add files via upload

---
+++ Day23.java
@@ -0,0 +1,13 @@
+public class Day23 {
+    public static void main(String[] args) {
+        // Latihan : Program persegi panjang
+        int panjang = 15;
+        int lebar = 10;
+
+        // Hitung luas
+        int luas = panjang * lebar;
+        System.out.println("Panjang = " + panjang);
+        System.out.println("Lebar   = " + lebar);
+        System.out.println("Luas Persegi Panjang = " + luas);
+    }
+}
