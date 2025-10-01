23b0a3a4c4da45f0cb530b4f9766be64d2ca0705
Daud <daud94237@gmail.com>
2025-10-01 20:45:52 +0800

    Day24.java

--- Day24.java
+++ Day24.java
@@ -2,10 +2,11 @@ public class Day24 {
     public static void main(String[] args) {
         // Latihan:Program luas lingkaran
         final double PHI = 3.14; 
-        double r = 5;
+        int r = 5;
         double luas = PHI * r * r;
 
         System.out.println("Luas lingkaran: " + luas) ;
     }
 
 }
+
