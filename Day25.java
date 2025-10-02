eaaf9aa36a56ce15fa43e4f9f0955ca4fe8e8141
Daud <daud94237@gmail.com>
2025-10-02 19:52:08 +0800

    Add files via upload

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class Day25 {
+    public static void main(String[] args) {
+        // Operator Increment dan Decrement (++, --)
+        int a = 10;
+        // increment
+        a++; 
+        System.out.println("Nilai a setelah di increment : " + a);
+        // decrement
+        a--; 
+        System.out.println("Nilai a setelah di decrement : " + a);
+        
+    }
+}
