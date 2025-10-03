bc273f990965d3a19e03b98e73ac4cda52ae64b2
Daud <daud94237@gmail.com>
2025-10-03 22:19:31 +0800

    Add files via upload

---
+++ Day26.java
@@ -0,0 +1,22 @@
+public class Day26 {
+    public static void main(String[] args) {
+        // Operator Penugasan (=, +=, -=, dll).
+         int a = 10;          
+        System.out.println("Nilai awal a = " + a);
+
+        a += 5;              
+        System.out.println("Setelah a += 5, a = " + a);
+
+        a -= 3;            
+        System.out.println("Setelah a -= 3, a = " + a);
+
+        a *= 2;     
+        System.out.println("Setelah a *= 2, a = " + a); 
+
+        a /= 4;           
+        System.out.println("Setelah a /= 4, a = " + a);
+
+        a %= 5;        
+        System.out.println("Setelah a %= 5, a = " + a);
+    }
+}
