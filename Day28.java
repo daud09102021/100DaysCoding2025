fa4dabf0afbe9fab070926bfad8819429e89ac60
Daud <daud94237@gmail.com>
2025-10-05 18:45:54 +0800

    Add files via upload

---
+++ Day28.java
@@ -0,0 +1,11 @@
+public class Day28 {
+    public static void main(String[] args) {
+        // Operator Perbandingan == dan !=
+
+        int a = 1;
+        int b = 2;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
