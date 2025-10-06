7b55f930ffa9f657b51313a5fa579df2436e66d1
Daud <daud94237@gmail.com>
2025-10-06 20:57:39 +0800

    Add files via upload

---
+++ Day29.java
@@ -0,0 +1,12 @@
+public class Day29 {
+    public static void main(String[] args) {
+        // Operator perbandingan < dan >
+        
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a < b);
+        System.out.println(a > b);
+
+    }
+}
