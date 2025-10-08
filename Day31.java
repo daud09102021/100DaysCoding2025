38d293411f98b50fbeb08fea0779f4783b5be16c
Daud <daud94237@gmail.com>
2025-10-08 22:41:40 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,9 @@
+public class Day31 {
+    public static void main(String[] args) {
+      //Operator Logika AND (&&)
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x && y);
+    }
+}
