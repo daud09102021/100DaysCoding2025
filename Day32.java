2737d345a6edab1c5863f9a2ab8bf0cf25115c77
Daud <daud94237@gmail.com>
2025-10-09 19:20:22 +0800

    Add files via upload

---
+++ Day32.java
@@ -0,0 +1,10 @@
+public class Day32 {
+    public static void main(String[] args) {
+        //Operator Logika OR (||).
+        
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a || b);
+
+    }
+}
