688ad3aae04582b90baa11587bd62c3bd8523bc6
Daud <daud94237@gmail.com>
2025-10-10 21:13:44 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,11 @@
+public class Day33 {
+    public static void main(String[] args) {
+        //Operator Logika NOT (!).
+        
+        boolean a = true;
+        boolean b = false;
+        System.out.println(!a);
+        System.out.println(!b);
+      
+    }
+}
