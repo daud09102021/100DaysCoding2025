e433d863a604b54363761f37605dcc2e0e576ab2
Daud <daud94237@gmail.com>
2025-10-11 21:33:34 +0800

    Day34.java

--- Day34.java
+++ Day34.java
@@ -1,6 +1,6 @@
 public class Day34 {
     public static void main(String[] args) {
-        
+        //Latihan: Mengkombinasikan berbagai operator.
         int a = 6;
         int b = 3;
         int c = 2;
