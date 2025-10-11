848e78a4116dd158e7b4fad51be927615934ac5d
Daud <daud94237@gmail.com>
2025-10-11 21:29:09 +0800

    Day34.java

---
+++ Day34.java
@@ -0,0 +1,12 @@
+public class Day34 {
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b = 3;
+        int c = 2;
+
+        boolean hasil = (a + b) > (b * c) && (a - c) != (b + c);
+
+        System.out.println("Hasil: " + hasil);
+    }
+}
