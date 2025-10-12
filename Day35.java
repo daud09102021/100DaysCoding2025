7132a4cfd16ea661076cf826ddfe18a10a773782
Daud <daud94237@gmail.com>
2025-10-12 20:51:00 +0800

    Add files via upload

---
+++ Day35.java
@@ -0,0 +1,11 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int nilai = 80;
+
+        if (nilai >= 75) {
+            System.out.println("Kamu Lulus");
+        } else {
+            System.out.println("Kamu Tidak Lulus");
+        }
+    }
+}
