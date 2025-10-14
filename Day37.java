26b73f9c1dee33b178e5539381712397273212f4
Daud <daud94237@gmail.com>
2025-10-14 22:55:51 +0800

    Add files via upload

---
+++ Day37.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day37 {
+    public static void main(String[] args) {
+        // 	Latihan: Menentukan bilangan ganjil atau genap
+        Scanner input = new Scanner(System.in);
+
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        }else {
+            System.out.println("Ganjil");
+        }
+    }
+}
