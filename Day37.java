7a2227ee2057ea6c23b87d49c30188422734ef5f
Daud <daud94237@gmail.com>
2025-10-14 23:50:13 +0800

    Day37.java

--- Day37.java
+++ Day37.java
@@ -6,8 +6,9 @@ public class Day37 {
         Scanner input = new Scanner(System.in);
 
         int angka = input.nextInt();
-
-        if (angka % 2 == 0) {
+        if (angka == 0) {
+            System.out.println("0");
+        }else if(angka % 2 == 0) {
             System.out.println("Genap");
         }else {
             System.out.println("Ganjil");
