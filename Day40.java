18dae4bcbf8df04575170e7ca283e2648a7af37f
Daud <daud94237@gmail.com>
2025-10-17 20:50:53 +0800

    Add files via upload

---
+++ Day40.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Day40 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+      
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = input.nextDouble();
+
+        double hasil;
+
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+        } else if (operator == '/') {
+            hasil = angka1 / angka2;
+                return;
+        } else {
+            System.out.println("Operator tidak valid!");
+            return;
+        }
+        System.out.println("Hasil: " + hasil);
+    }
+}
