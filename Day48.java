a996450c63e28a85ce36b96b8dfc41337fbd89aa
Daud <daud94237@gmail.com>
2025-10-25 21:13:51 +0800

    Add files via upload

---
+++ Day48.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Day48 {
+    public static void main(String[] args) {
+        // Latihan: Membuat Kalkulator Menggunakan Switch-case di java
+        Scanner input = new Scanner(System.in);
+        double hasil;
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = input.nextDouble();
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = input.nextDouble();
+
+        System.out.print("Pilih operator +, -, *, /, %: ");
+        char operator = input.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '%':
+                hasil = angka1 % angka2;
+                System.out.println("Hasil:" + hasil);
+                break;
+            default:
+                System.out.println("Operator tidak dikenal!");
+        }
+
+        input.close();
+    }
+}
