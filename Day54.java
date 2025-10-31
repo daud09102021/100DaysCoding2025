13348644f50a3a35f239a116bc5e44a56af56386
Daud <daud94237@gmail.com>
2025-10-31 21:34:03 +0800

    Add files via upload

---
+++ Day54.java
@@ -0,0 +1,11 @@
+public class Day54 {
+    public static void main(String[] args) {
+        // keyword continue
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue; 
+            }
+            System.out.println(i);
+        }
+    }
+}
