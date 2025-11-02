43cbebada86d0d3cfbdfe8e299a343cf907d9c13
Daud <daud94237@gmail.com>
2025-11-02 22:40:28 +0800

    Add files via upload

---
+++ Day56.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Day56 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String username, password;
+        int percobaan = 0;
+        boolean login = false;
+
+        do {
+            System.out.print("Username : ");
+            username = input.nextLine();
+            System.out.print("Password : ");
+            password = input.nextLine();
+
+            if (username.equals("Auliyah") && password.equals("111")) {
+                System.out.println("Login Sebagai Pengguna");
+                login = true;
+            } 
+            else if (username.equals("admin") && password.equals("admin")) {
+                System.out.println("Anda Login Sebagai Admin");
+                login = true;
+            } 
+            else {
+                System.out.println("Silahkan Coba Lagi");
+                percobaan++;
+            }
+
+        } while (percobaan < 3 );
+            System.out.println("Login Gagal");
+        
+    }
+}
