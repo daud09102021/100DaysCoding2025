d003a7adabe5936cb55f6deae03e1b54d404ceea
Daud <daud94237@gmail.com>
2025-11-05 22:08:11 +0800

    Update Day59.java
    Refactor output formatting for even and odd numbers.

--- Day59.java
+++ Day59.java
@@ -4,19 +4,21 @@ public class Day59 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-
         for (int i = 1; i <= N; i++) {
             if (i % 2 == 0) {
                 System.out.print(i);
-                if (i < N - 1) System.out.print(" - ");
+                if (i + 2 <= N) {
+                    System.out.print(" - ");
+                }
             }
         }
-       System.out.println();
-
+        System.out.println(); 
         for (int i = N; i >= 1; i--) {
-            if (i % 2 == 1) {
+            if (i % 2 != 0) {
                 System.out.print(i);
-                if (i > 2) System.out.print(" - ");
+                if (i - 2 >= 1) {
+                    System.out.print(" - ");
+                }
             }
         }
     }
