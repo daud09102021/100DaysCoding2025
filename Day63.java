dcf8b0ad4dfb07458bfc4a90353944e52b9429f5
Daud <daud94237@gmail.com>
2025-11-09 22:01:14 +0800

    Add files via upload

---
+++ Day63.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day63 {
+     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Nilai N : ");
+        int N = input.nextInt();
+        int jumlah = 1;
+        for (int i = 1; i <= N; i++) {
+            jumlah *= i;
+            System.out.print(i);
+            if (i < N) {
+                System.out.print("*");
+            }
+        }
+
+        System.out.println(" = " + jumlah);
+    }
+}
