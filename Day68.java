259012077d72df99bffd3a2b7a72517a4b69af3e
Daud <daud94237@gmail.com>
2025-11-14 22:53:13 +0800

    Add files via upload

---
+++ Day68.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day68 {
+    public static void main(String[] args) {
+        Scanner aa = new Scanner(System.in);
+        System.out.print("Masukkan angka : ");
+        int N = aa.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N ; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+}
