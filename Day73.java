8b473edd249abecef01e140408e7dca73d1327fb
Daud <daud94237@gmail.com>
2025-11-19 21:32:18 +0800

    Update Day73.java
    Refactor loop to use for (;;) and handle negative input correctly.

--- Day73.java
+++ Day73.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class Day73 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         int total = 0;
-        int angka;
 
-        while (true) {
+        for (;;) {
             System.out.print("Masukkan angka : ");
-            angka = input.nextInt();
+            int angka = input.nextInt();
 
-            total += angka; 
-
+           
             if (angka < 0) {
+                total += Math.abs(angka); 
                 break; 
             }
+
+            total += angka;
         }
 
         System.out.println("Hasil : " + total);
