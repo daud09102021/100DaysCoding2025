def705bbeec0291454ffad7f2d43903773a9da29
Daud <daud94237@gmail.com>
2025-11-27 21:49:06 +0800

    Add files via upload

---
+++ Day81.java
@@ -0,0 +1,10 @@
+public class Day81 {
+    public static void main(String[] args) {
+
+        int[] nilai = {70, 80, 90, 85, 75, 65, 50, 15, 20};
+
+        System.out.println("Nilai pada indeks 2 : " + nilai[2]);
+        System.out.println("Nilai pada indeks 5 : " + nilai[5]);
+        System.out.println("Nilai pada indeks 7 : " + nilai[7]);
+    }
+}
