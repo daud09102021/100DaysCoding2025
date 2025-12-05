8ab32f2c4a8eee51dc2e3c54807101c2cfd27ccd
Daud <daud94237@gmail.com>
2025-12-05 22:47:02 +0800

    Add files via upload

---
+++ Day89.java
@@ -0,0 +1,11 @@
+public class Day89 {
+
+    
+    static void sapa() {
+        System.out.println("Halo, selamat datang");
+    }
+
+    public static void main(String[] args) {
+        sapa();  
+    }
+}
