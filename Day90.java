939ab1242df220a6930995db6d9f324bbd3d2e46
Daud <daud94237@gmail.com>
2025-12-06 22:38:52 +0800

    Add files via upload

---
+++ Day90.java
@@ -0,0 +1,10 @@
+public class Day90 {
+    int kali(int a, int b) {
+        return a * b;  
+    }
+    public static void main(String[] args) {
+        Day90 obj = new Day90();
+        int hasil = obj.kali(4, 5);
+        System.out.println("Hasil perkalian: " + hasil);
+    }
+}
