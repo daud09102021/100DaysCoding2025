c11c979117ba14de67c784948d4dce75d5701db3
Daud <daud94237@gmail.com>
2025-12-07 22:02:38 +0800

    Add files via upload

---
+++ Day91.java
@@ -0,0 +1,11 @@
+public class Day91 {
+    void sapa(String nama) {
+        System.out.println("Halo, " + nama);
+    }
+
+    public static void main(String[] args) {
+        Day91 obj = new Day91();
+
+        obj.sapa("Daud"); 
+    }
+}
