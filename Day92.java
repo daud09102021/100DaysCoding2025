2fcf94620cd7519f2645ecc8346734cacd6b8ee0
Daud <daud94237@gmail.com>
2025-12-08 22:27:52 +0800

    Add files via upload

---
+++ Day92.java
@@ -0,0 +1,12 @@
+public class Day92 {
+    int tambah(int a, int b) {
+        return a + b;  
+    }
+    public static void main(String[] args) {
+
+        Day92 obj = new Day92();
+
+        int hasil = obj.tambah(5, 3);  
+        System.out.println("Hasil = " + hasil);
+    }
+}
