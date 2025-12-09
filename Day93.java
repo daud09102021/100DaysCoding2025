2c8167d5968ee7f2bffa033b742b98eb2ae81a99
Daud <daud94237@gmail.com>
2025-12-09 22:12:14 +0800

    Add files via upload

---
+++ Day93.java
@@ -0,0 +1,9 @@
+public class Day93 {
+    public static void main(String[] args) {
+        double x = 4.5;
+
+        System.out.println("Ceil  : " + Math.ceil(x));   
+        System.out.println("Floor : " + Math.floor(x));  
+        System.out.println("Round : " + Math.round(x)); 
+    }
+}
