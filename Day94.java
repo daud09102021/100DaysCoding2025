ca035c9d930686392caf7447199825f2213a9e53
Daud <daud94237@gmail.com>
2025-12-10 22:46:53 +0800

    Add files via upload

---
+++ Day94.java
@@ -0,0 +1,10 @@
+public class Day94 {
+    void cetakTulisan() {
+        System.out.println("Ini adalah method void!");
+    }
+
+    public static void main(String[] args) {
+        Day94 a = new Day94();
+        a.cetakTulisan(); 
+    }
+}
